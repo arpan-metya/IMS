4f2c671eed1467e2b17807b796963969d71225a1
Arpan Metya <arpanmetya.1@gmail.com>
2023-09-12 23:59:53 +0530

    defaults.py file is added for control of default settings of the app.

---
+++ defaults.py
@@ -0,0 +1,24 @@
+DEFAULT_WINDOW_APPEARANCE = 'dark'		# ('light', 'dark')
+
+DEFAULT_HIDE_TITLEBAR = False
+
+DEFAULT_FULLSCREEN = False
+
+DEFAULT_ANIMATION = True
+
+DEFAULT_MENU_BUTTON = 'profile'		# ('profile', 'product')
+
+DEFAULT_CONTROL_BUTTON = 'profile'		# (('profile', 'user', 'employee', 'settings'), ('dashboard', 'product', 'update'))
+
+DEFAULT_LOG_STATUS = ('logged out', 'denied')		# (('logged in', 'logged out'), ('admin', 'write', 'read', 'denied'))
+
+DEFAULT_DATABASE = 'inventory.db'
+
+# openpyxl
+
+# light white = #ececec, #d1d1d1
+# light black = #111111, #2e2e2e
+# dark mode purple = #d437cc
+# dark mode purple hover = #a827a2
+# light mode blue = #36a4ff
+# light mode blue hover = #6ebbfa
